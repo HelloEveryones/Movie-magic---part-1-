5db5eb52f42cd4186cd207de33c31e1311f1549d
Iva <iva_dkostadinova@abv.bg>
2024-01-29 15:12:12 +0200

    Add movie id's and details link

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,18 +1,20 @@
-const movies= [ {
-    title: 'iva',
-    genre: 'iva',
-    director: 'iva',
-    date: '2015',
-    imageUrl: 'iva',
-    rating: '6',
-    description: 'iva iva iva '
-  }
-  
-  ];
+const movies = [
+  {
+    _id: 1,
+    title: "iva",
+    genre: "iva",
+    director: "iva",
+    date: "2015",
+    imageUrl: "iva",
+    rating: "6",
+    description: "iva iva iva ",
+  },
+];
 
-  exports.getAll=()=>{
-    return movies.slice();
-  }
-exports.create=(movieData)=>{
-    movies.push(movieData);
-}
+exports.getAll = () => {
+  return movies.slice();
+};
+exports.create = (movieData) => {
+  movieData._id = movies[movies.length - 1]._id + 1;
+  movies.push(movieData);
+};
