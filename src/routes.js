0d22565a38066a842265912e01174c5e7b3ad151
Iva <iva_dkostadinova@abv.bg>
2024-01-29 10:57:55 +0200

    Add page 404

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,11 @@ const router = require("express").Router();
 const homeController = require("./controllers/homeController");
 const movieController=require("./controllers/movieController");
 
-
-router.use(homeController);
 router.use(movieController);
+router.use(homeController);
+
+router.get('*', (req, res)=>{
+    res.render("/404")
+})
 
 module.exports = router;
