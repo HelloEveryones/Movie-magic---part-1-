2e4666edb8a4850a87dbe4fe7515b3c9f115e382
Iva <iva_dkostadinova@abv.bg>
2024-01-29 12:19:02 +0200

    extract expres and handlebars config

---
+++ src/config/configExpress.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require("path");
+
+function configExpress(app){
+    app.use(express.static(path.join(__dirname,"public")))
+    return app;
+}
+module.exports= configExpress;
