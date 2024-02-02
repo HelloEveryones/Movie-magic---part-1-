e2af90c9168acecc7b6064af948df7f0712ad965
Iva <iva_dkostadinova@abv.bg>
2024-02-02 12:31:54 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
-
-const routes=require('./routes');
-const configHandlebars=require('./config/configHandlebars');
-const configExpress=require("./config/configExpress")
+const routes = require("./routes");
+const configHandlebars = require("./config/configHandlebars");
+const configExpress = require("./config/configExpress");
 
 const app = express();
 const port = 5000;
 
+// Configure middleware
 configHandlebars(app);
 configExpress(app);
 
+// Use the defined routes
 app.use(routes);
-app.listen(port, () => console.log(`Server is listening on port ${port}`));
+
+mongoose.connect('mongodb://localhost:27017/movies')
+  .then(() => {
+    console.log('DB Connected!');
+    app.listen(port, () => console.log(`Server is listening on port ${port}`));
+  })
+  .catch((err) => {
+    console.error('Error connecting to DB:', err.message);
+  });
